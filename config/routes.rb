817504a9def139f95a9c022053598e5e43df297a
Anthony Oglesby <ogle1ant@gmail.com>
2020-02-09 16:26:07 -0500

    added routs, signup form, and show page for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
+
+  get '/signup' => 'users#new'
+
+
   resources :taskfeeds
   resources :nagfeeds
   resources :groups
   resources :nags
   resources :tasks
   resources :users
-  resources :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
