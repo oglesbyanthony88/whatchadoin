4235e0819363eff8562950af534fe833799411d5
Anthony Oglesby <ogle1ant@gmail.com>
2020-02-10 08:31:17 -0500

    added bulma-rails gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,17 @@ class SessionsController < ApplicationController
 		redirect_to root_path
 	end
 
+	def create
+		user = User.find_by(email: params[:user][:email])
+			if user && user.authenticate(params[:user][:password])
+				session[:user_id] = user.id
+				redirect_to user_path(user)
+			else
+				flash[:message] = "Incorrect Login Info"
+				redirect_to login_path
+			end
+	end
+
+
+
 end
