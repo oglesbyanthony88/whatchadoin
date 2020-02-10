028291d8dd7734a921d3849f26602e1c0ece3a65
Anthony Oglesby <ogle1ant@gmail.com>
2020-02-10 12:11:11 -0500

    added index method for nags

--- app/controllers/nags_controller.rb
+++ app/controllers/nags_controller.rb
@@ -1,4 +1,33 @@
 class NagsController < ApplicationController
 	before_action :redirect_if_not_logged_in #See App Controller method
 
+	#creates new nag
+	def new
+			@nag = Nag.new
+	end
+	#Creates a nag and redirects to User Nags (INDEX)
+	# unless the nag does not save
+	#Need to put a min character count on Nags!
+
+	def create
+		@nag = current_user.nags.build(nag_params)
+		if @nag.save
+			redirect_to nags_path
+		else
+			render :new
+			#binding.pry
+		end
+	end
+
+	def index
+   @nags = current_user.nags.all
+  end
+
+	private
+
+	def nag_params
+		params.require(:nag).permit(:content)
+	end
+
+
 end
