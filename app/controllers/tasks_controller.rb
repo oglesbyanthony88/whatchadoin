d192a3d7fd5cdad7e7ba9fb62f86e41ee4b1a4bb
Anthony Oglesby <ogle1ant@gmail.com>
2020-02-10 12:12:51 -0500

    app/controllers/tasks_controller.rb
    building out tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
 class TasksController < ApplicationController
+
+def new
+	@task = Task.new
 end
+	#Creates a task and redirects to User tasks (INDEX)
+	# unless the task does not save
+	#Need to put a min character count on tasks!
+	
+	def create
+		@task = current_user.tasks.build(task_params)
+		if @task.save
+			redirect_to tasks_path
+		else
+			render :new
+			#binding.pry
+		end
+	end
+
+	private
+
+	def task_params
+		params.require(:task).permit(:content)
+	end
+
+
+end
+
