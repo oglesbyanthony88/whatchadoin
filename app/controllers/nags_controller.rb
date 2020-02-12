9274157644ca3282fcde2e72aab5f3dab591e410
Anthony Oglesby <ogle1ant@gmail.com>
2020-02-12 16:19:48 -0500

    Nags index for admins/users. Nags can be deleted by both.

--- app/controllers/nags_controller.rb
+++ app/controllers/nags_controller.rb
@@ -47,6 +47,20 @@ class NagsController < ApplicationController
     end
   end
 
+  def destroy
+   	if current_admin
+    	@nag = current_admin.nags.find(params[:id])
+    	@nag.destroy
+    	redirect_to admin_path(current_admin)
+    elsif current_user
+    	@nag = current_user.nags.find(params[:id])
+    	@nag.destroy
+    	redirect_to user_path(current_user)
+    else 
+    	redirect_to root_path
+    end
+  end
+
 	private
 
 	def nag_params
